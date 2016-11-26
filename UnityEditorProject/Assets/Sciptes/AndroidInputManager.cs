f113afe0d0257835d083e73630922ef2cea67132
BlackZIjian <735809461@qq.com>
2016-11-26 17:06:49 +0800

    AddAndroidSensor
    添加安卓传感器支持，重新整理文件结构

---
+++ UnityEditorProject/Assets/Sciptes/AndroidInputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class AndroidInputManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        aaa();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void aaa()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("RegisterSensor", 0);
+        
+    }
+
+}
+
---
+++ UnityEditorProject/Assets/Sciptes/MoveModel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveModel : MonoBehaviour {
+    public Transform m_model;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(CheckShaked())
+        {
+            m_model.position = transform.position;
+            m_model.rotation = transform.rotation;
+        }
+	}
+
+    bool CheckShaked()
+    {
+        return true;
+    }
+
+    public void OnFoundModel()
+    {
+
+    }
+
+    public void OnLostModel()
+    {
+
+    }
+}
